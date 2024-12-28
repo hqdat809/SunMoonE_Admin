f6adfc55dbcbaa26edfe5212a4e6a308abb814f0
dat.haquoc <dat.haquoc@ncc.asia>
2024-12-28 10:02:37 +0700

    refactor code

--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -5,34 +5,18 @@ import {
     DialogTitle,
     TextField
 } from "@mui/material";
-import _, { filter } from "lodash";
-import { useCallback, useEffect, useState } from "react";
-import Table from "../../components/table/Table";
+import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
 import { userColumns } from "../../components/table/table-data";
-import {
-    ICustomerRequest
-} from "../../interfaces/customer-interface";
 import { IUserBank, IUserData, RoleEnum } from "../../interfaces/user-interfaces";
 import * as UserService from "../../services/user-service";
 import { toastError, toastSuccess } from "../../utils/notifications-utils";
 import "./UserPage.scss";
-import { DataGrid } from "@mui/x-data-grid";
 
 const Customers = () => {
-    const [total, setTotal] = useState(0);
-    const [loading, setLoading] = useState<boolean>(false);
     const [users, setUsers] = useState<IUserData[]>([]);
     const [open, setOpen] = useState(false);
     const [bankDetails, setBankDetails] = useState<IUserBank | undefined>();
-
-    const [filtered, setFiltered] = useState<ICustomerRequest>({
-        pageSize: 20,
-        currentItem: 0,
-        orderBy: "email",
-        orderDirection: "ASC",
-    });
-
-
 
     const handleOpenBankDialog = (bank: IUserBank) => {
         setOpen(true);
@@ -48,10 +32,6 @@ const Customers = () => {
         const users = await UserService.getAllUser();
 
         setUsers(users.data);
-        setTotal(users.data.length);
-        setTimeout(() => {
-            setLoading(false);
-        }, 700);
     };
 
     const handleUpdateUserRole = async (userId: string, userRole: RoleEnum) => {
@@ -60,22 +40,13 @@ const Customers = () => {
             toastSuccess("Cập nhật hạng người dùng thành công")
             await handleGetCustomer()
         } catch (error) {
-            toastError("Cập nhật hạng người dùng thất bại")
+            toastError("Cập nhật hạng người dùng thất bại: ", error)
         }
     }
 
-    const handleSetCurrentItem = useCallback(
-        _.debounce((index) => {
-            setFiltered({ ...filtered, currentItem: index });
-        }, 500),
-        [filtered]
-    );
-
     useEffect(() => {
-        console.log('change')
-        setLoading(true);
         handleGetCustomer();
-    }, [filtered]);
+    }, []);
 
     return (
         <div className="page-container">
